dbd39f985ee6287d220a7f916d02194a3dc9fef7
MadickAngeCesar <madick.angecesar@ictuniversity.edu.cm>
2024-11-05 16:57:16 +0100

    Completing documentation

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,8 @@
-export default function Page() {
-    return <p>Customers Page</p>;
-  }
+import CustomersTable from "@/app/ui/customers/table";
+import React from "react";
+
+const CustomersPage: React.FC = () => {
+  return <CustomersTable customers={[]} />;
+};
+
+export default CustomersPage;
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    appDir: true,
+    fontLoaders: [
+      {
+        loader: '@next/font/google',
+        options: { subsets: ['latin'] },
+      },
+    ],
+  },
 };
 
 export default nextConfig;
